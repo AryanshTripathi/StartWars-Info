f5cb074bb44ed238204767a9844e5b61075a7100
AryanshTripathi <aryansh.tripathi.civ20@itbhu.ac.in>
2022-06-29 18:36:58 +0530

    centered the spinner

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -17,26 +17,22 @@ const Planets = ({ BallTriangle }) => {
 	console.log(data);
 	return (
 		<div>
-			<h2>People</h2>
-
-			<button onClick={() => setPage(1)}>Page 1</button>
-			<button onClick={() => setPage(2)}>Page 2</button>
-			<button onClick={() => setPage(3)}>Page 3</button>
-			<button onClick={() => setPage(4)}>Page 4</button>
-			<button onClick={() => setPage(5)}>Page 5</button>
-			<button onClick={() => setPage(6)}>Page 6</button>
-			<button onClick={() => setPage(7)}>Page 7</button>
-			<button onClick={() => setPage(8)}>Page 8</button>
-			<button onClick={() => setPage(9)}>Page 9</button>
-
-			{status == "loading" && (
-				<div className="ball-triangle">
-					<BallTriangle />
-				</div>
-			)}
+			{status == "loading" && <BallTriangle />}
 			{status == "error" && <div>Error fetching data</div>}
 			{status == "success" && (
 				<div>
+					<h2>People</h2>
+					<div className="btn-group">
+						<button onClick={() => setPage(1)}>Page 1</button>
+						<button onClick={() => setPage(2)}>Page 2</button>
+						<button onClick={() => setPage(3)}>Page 3</button>
+						<button onClick={() => setPage(4)}>Page 4</button>
+						<button onClick={() => setPage(5)}>Page 5</button>
+						<button onClick={() => setPage(6)}>Page 6</button>
+						<button onClick={() => setPage(7)}>Page 7</button>
+						<button onClick={() => setPage(8)}>Page 8</button>
+						<button onClick={() => setPage(9)}>Page 9</button>
+					</div>
 					{data.results.map((individual, index) => (
 						<Individual individual={individual} key={index} />
 					))}
--- src/Components/Planets.jsx
+++ src/Components/Planets.jsx
@@ -15,23 +15,19 @@ const Planets = ({ BallTriangle }) => {
 
 	return (
 		<div>
-			<h2>Planets</h2>
-
-			<button onClick={() => setPage(1)}>Page 1</button>
-			<button onClick={() => setPage(2)}>Page 2</button>
-			<button onClick={() => setPage(3)}>Page 3</button>
-			<button onClick={() => setPage(4)}>Page 4</button>
-			<button onClick={() => setPage(5)}>Page 5</button>
-			<button onClick={() => setPage(6)}>Page 6</button>
-
-			{status == "loading" && (
-				<div className="ball-triangle">
-					<BallTriangle />
-				</div>
-			)}
+			{status == "loading" && <BallTriangle />}
 			{status == "error" && <div>Error fetching data</div>}
 			{status == "success" && (
 				<div>
+					<h2>Planets</h2>
+					<div className="btn-group">
+						<button onClick={() => setPage(1)}>Page 1</button>
+						<button onClick={() => setPage(2)}>Page 2</button>
+						<button onClick={() => setPage(3)}>Page 3</button>
+						<button onClick={() => setPage(4)}>Page 4</button>
+						<button onClick={() => setPage(5)}>Page 5</button>
+						<button onClick={() => setPage(6)}>Page 6</button>
+					</div>
 					{data.results.map((planet, index) => (
 						<Planet planet={planet} key={index} />
 					))}
